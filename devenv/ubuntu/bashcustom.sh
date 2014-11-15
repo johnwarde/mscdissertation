ae0e67cfac1571241e62577cceae476186c8f004
John Warde <john.warde@gmail.com>
2014-11-15 17:52:39 +0000

    Initial version on bashcustom.sh for shell customisation

---
+++ devenv/ubuntu/bashcustom.sh
@@ -0,0 +1,14 @@
+#!/bin/bash         
+# ~/scm/mscdissertation/devenv/ubuntu/.bash_custom
+# Ubuntu terminal customisation
+
+# Add the next few lines to ~/.bashrc so that this customisation get executed
+## johnwarde: added for own customisation
+#if [ -f ~/scm/mscdissertation/devenv/ubuntu/bashcustom.sh ]; then
+#    . ~/scm/mscdissertation/devenv/ubuntu/bashcustom.sh
+#fi
+
+alias devenv='cd ~/scm/mscdissertation/devenv/ubuntu'
+alias research='cd ~/scm/mscdissertation/research'
+
+
