77224d609ec7a4880cee6a3daa1681dfb7d73ab5
John Warde <john.warde@gmail.com>
2014-11-15 00:32:09 +0000

    Initial version of Ubunt4MscDissertation.rb which will manage the development environment incase a VM goes down

---
+++ devenv/ubuntu/Ubuntu4MscDissertation.rb
@@ -0,0 +1,19 @@
+# To run this script type ...
+# chef-apply
+# As I learn more about chef I should be able to create a cookbook etc.
+
+package 'tree'
+package 'git-all'
+# The following may have only installed the basic version of Eclipse, use apt-cache search <packagename> | grep <packagename>
+package 'eclipse'
+
+
+
+
+# Have installed Google Chrome Browser manually through Firefox browser which came with Ubuntu
+# Google Chrome Browser is not in the apt-cache or in the Ubuntu Software Center.  Probably will need a custom chef receipe (?) using the steps package is not in repositories, will need to figure out custom configuration in Chef to run commands in http://askubuntu.com/questions/79280/how-to-install-chrome-browser-properly-via-command-line
+# sudo apt-get install libxss1 libappindicator1 libindicator7
+# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# sudo dpkg -i google-chrome*.deb
+# ... becomes 'google-chrome-stable' after installation
+
