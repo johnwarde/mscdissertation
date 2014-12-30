0a478c2941a5e5b8b7c289f68a7896d5fa1eb1a8
John Warde <john.warde@gmail.com>
2014-12-30 00:43:02 +0000

    Added maven and other info

--- devenv/ubuntu/Ubuntu4MscDissertation.rb
+++ devenv/ubuntu/Ubuntu4MscDissertation.rb
@@ -7,9 +7,15 @@ package 'xclip'
 package 'git-all'
 # The following may have only installed the basic version of Eclipse, use apt-cache search <packagename> | grep <packagename>
 package 'eclipse'
+package 'maven'
 
 
+# For Eclipse, install the following plugins (add sites below to ) ...
+# Maven Integration http://download.eclipse.org/technology/m2e/releases
+# EGit http://download.eclipse.org/egit/updates
 
+# To uninstall a package, use the following ... (haven't had to try yet)
+# sudo apt-get remove <packagename>
 
 # Have installed Google Chrome Browser manually through Firefox browser which came with Ubuntu
 # Google Chrome Browser is not in the apt-cache or in the Ubuntu Software Center.  Probably will need a custom chef receipe (?) using the steps package is not in repositories, will need to figure out custom configuration in Chef to run commands in http://askubuntu.com/questions/79280/how-to-install-chrome-browser-properly-via-command-line
@@ -17,4 +23,14 @@ package 'eclipse'
 # wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 # sudo dpkg -i google-chrome*.deb
 # ... becomes 'google-chrome-stable' after installation
+# Has to be started from command line using ...
+# google-chrome &
+
+
+
+
+
+# Other
+# use 'printenv' to see you environment variables
+
 
