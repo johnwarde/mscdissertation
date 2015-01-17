60ac5dfcc38b4d53884faeb28b4edc50726d2c84
John Warde <john.warde@gmail.com>
2015-01-17 18:34:47 +0000

    Added authorized users to database; Added xmlns:sec to HTML pages to enable logic on authorization/authentication.

--- workspace/webapp/src/main/resources/data.sql
+++ workspace/webapp/src/main/resources/data.sql
@@ -1,3 +1,7 @@
 insert into users (username, password, enabled) values ('user', 'user', true);
+insert into users (username, password, enabled) values ('jw', 'jw', true);
+insert into users (username, password, enabled) values ('autogen', 'autogen', true);
 
 insert into authorities (username, authority) values ('user', 'ROLE_ADMIN');
+insert into authorities (username, authority) values ('jw', 'ROLE_ADMIN');
+insert into authorities (username, authority) values ('autogen', 'ROLE_ADMIN');
