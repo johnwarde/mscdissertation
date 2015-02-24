55652b313fe2295d91ca489e7825128c7c9bc585
John Warde <john.warde@gmail.com>
2015-02-24 23:49:16 +0000

    Clean up before tag & branch (again)
    Updates to infra notes; aws scripts and pre-experiment test run data and
    graph

--- devenv/ubuntu/aws-ec2-base.sh
+++ devenv/ubuntu/aws-ec2-base.sh
@@ -2,20 +2,28 @@
 # ~/mscdissertation/devenv/ubuntu/aws-ec2-base.sh
 # Sets up software needed to run the reference application components on an Amazon Web Service EC2 instance 
 
-# NOTE: cannot run this yet as some of the apt-get command ask to confirm that you want to install, are there options?
+
+
+# NOTE: the '-y' option allow us to run unattended
 
 # Install AWS Command Line
-sudo apt-get install awscli
+sudo apt-get install awscli -y
 
 # Install Java
-sudo add-apt-repository ppa:webupd8team/java 
-sudo apt-get update 
-sudo apt-get install oracle-java7-installer 
-sudo apt-get install oracle-java7-set-default
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get update -y
+sudo apt-get install oracle-java7-installer -y       # We still need to press OK for this package
+sudo apt-get install oracle-java7-set-default -y
 
 # Install RabbitMQ Server
-sudo apt-get install rabbitmq-server
+sudo apt-get install rabbitmq-server -y
+sudo rabbitmq-plugins enable rabbitmq_management
+# Need to stop and start RabbitMQ to enable the WebUI
+sudo rabbitmqctl stop
+sudo invoke-rc.d rabbitmq-server start
 
+# Optionally
+sudo apt-get install unzip -y
 
 # Verify installed software
 echo ---------------------------------------
@@ -23,11 +31,28 @@ java -version
 javac -version 
 sudo rabbitmqctl list_exchanges
 
+
+# Configuring the AWS Command Line Interface
+# http://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html
+# Can set environment variables to AccessID, SecretKey but needs to be done through a startup .bashrc (save into an AMI)
 # Configure AWS CLI (enter AccessID, SecretKey, region = us-east-1a, format = json)
 aws configure 
 
 
 # Copy JARs from S3
+aws s3 ls s3://johnwarde.net/
 aws s3 cp s3://johnwarde.net/processor-0.0.1-SNAPSHOT.jar .
 aws s3 cp s3://johnwarde.net/webapp-1.2.2.BUILD-SNAPSHOT.jar .
+aws s3 cp s3://johnwarde.net/webappresourcestest.zip .
+unzip webappresourcestest.zip -d .
+
+cd processor
+java -jar processor-0.0.1-SNAPSHOT.jar
+
+cd webserver1
+java -jar webapp-1.2.2.BUILD-SNAPSHOT.jar
+
+cd webserver2
+java -jar webapp-1.2.2.BUILD-SNAPSHOT.jar
+
 
