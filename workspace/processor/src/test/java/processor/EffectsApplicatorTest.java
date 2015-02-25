f29f34e25e8249c71f7538c2dce3781b987605bc
John Warde <john.warde@gmail.com>
2015-02-25 18:30:53 +0000

    Add test file so that git adds the src/test folder which is required to build - this was missing when I created a new working folder for branching

---
+++ workspace/processor/src/test/java/processor/EffectsApplicatorTest.java
@@ -0,0 +1,14 @@
+package processor;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EffectsApplicatorTest {
+
+	@Test
+	public void testApply() {
+		fail("Not yet implemented");
+	}
+
+}
